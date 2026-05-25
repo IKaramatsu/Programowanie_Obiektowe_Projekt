84551283f1faada1f0a3d23ece6920924a4879be
HaniaK0 <kedzierska.hana@gmail.com>
2026-05-25 19:14:31 +0200

    zrobiony (?) Cell + coreDeath

---
+++ src/cell/Cell.java
@@ -0,0 +1,61 @@
+package cell;
+import java.util.ArrayList;
+import java.util.List;
+
+import astrophage.Astrophage;
+
+public class Cell {
+    private int x, y, regeneration;
+    private List<Astrophage> agents;
+    private float brightness;
+    private boolean core, shadow;
+
+    public Cell(int x, int y, List<Astrophage> agents, float brightness, boolean core, boolean shadow, int regeneration){
+        this.x = x;
+        this.y = y;
+        this.agents = new ArrayList<>();
+        this.brightness = brightness;
+        this.core = core;
+        this.shadow = shadow;
+        this.regeneration = regeneration;
+    }
+
+    public boolean isEmpty(){
+        if (agents.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setCore(boolean core){
+        this.core = core;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public List<Astrophage> getAgents(){
+        return agents;
+    }
+
+    public float getBrightness(){
+        return brightness;
+    }
+
+    public boolean getCore(){
+        return core;
+    }
+
+    public boolean getShadow(){
+        return shadow;
+    }
+
+    public int getRegeneration(){
+        return regeneration;
+    }
+}
