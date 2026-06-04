c0dbad96269ace5daa8f63b6f3319f58110811b2
IKaramatsu <xxigamodxx@gmail.com>
2026-06-04 20:37:50 +0200

    Fixed inheritance
    Fixed inheritance

--- src/astrophage/Inheritance.java
+++ src/astrophage/Inheritance.java
@@ -4,6 +4,8 @@ import simulation.RandomVariables;
 
 public class Inheritance {
     public static void inheritance(Astrophage child, Astrophage parent) {
+        child.setX(parent.getX());
+        child.setY(parent.getY());
         child.setEnergy(RandomVariables.inheritanceEnergyRandom(parent));
         child.setEnergyMax(RandomVariables.inheritanceEnergyMaxRandom(parent));
         child.setAgeMax(RandomVariables.inheritanceAgeMaxRandom(parent));
